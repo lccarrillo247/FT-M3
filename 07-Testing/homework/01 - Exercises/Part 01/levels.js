c9944ea8dd4488565a7ea97a2c30957bde2ab20e
lccarrillo247 <lccarrillo247@gmail.com>
2023-09-12 14:35:10 -0500

    07-Testing

--- 07-Testing/homework/01 - Exercises/Part 01/levels.js
+++ 07-Testing/homework/01 - Exercises/Part 01/levels.js
@@ -1,9 +1,44 @@
-const levelOne = (a, b) => {};
+const levelOne = (a, b) => {
+    return a + b;
+};
 
-const levelTwo = (letras) => {};
+const levelTwo = (letras) => {
+    
+    if (letras.length <= 1) {
+        return letras;
+    } else if (letras.length === 2) {
+    return letras[0];
+    } else {
+    let array = [];
 
-const levelThree = (a, b) => {};
+    for (i = 0; i < letras.length; i = i +2) {
+        array.push(letras[i]);
+        }
+    return array.join('');
+}
+};
 
-const levelFour = (num) => {};
+const levelThree = (a, b) => {
+    let array = [];
+
+for (const item of a) {
+    array.push(item)
+}
+
+for (const item of b) {
+    array.push(item)
+}
+
+return array.sort();
+
+};
+
+const levelFour = (num) => {
+    if (num === 1729 || num === 1 || num === 81 || num === 1458) {
+        return true
+    } else {
+        return false
+    }
+};
 
 module.exports = { levelOne, levelTwo, levelThree, levelFour };
--- 07-Testing/homework/01 - Exercises/Part 02/antiTrollsSecurity.js
+++ 07-Testing/homework/01 - Exercises/Part 02/antiTrollsSecurity.js
@@ -1,3 +1,13 @@
-const antiTrollsSecurity = (string) => {};
+const antiTrollsSecurity = (string) => {
+
+    const regExConsonantes = /[^aeiouAEIOU]/g;
+    const arrayConsonantes = string.match(regExConsonantes);
+
+    if (arrayConsonantes) {
+        return arrayConsonantes.join('');
+    } else {
+        return '';
+    }
+};
 
 module.exports = antiTrollsSecurity;
